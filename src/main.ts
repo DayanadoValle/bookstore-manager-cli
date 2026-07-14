78c04a137a9ce2d17879c6ab7e441f0b8e5a6deb
DayanadoValle <dayanadovalle@hotmail.com>
2026-07-14 20:02:55 -0300

    feat: configura conexão com PostgreSQL #2

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Iniciando BookStore CLI da Day..");
+import { testarConexao, pool } from "./database/connection.js";
+
+async function main(): Promise<void> {
+  console.log("Iniciando BookStore CLI da Day..");
+
+  await testarConexao();
+
+  console.log("Banco conectado com sucesso!");
+
+  await pool.end();
+}
+
+main().catch((error) => {
+  console.error((error as Error).message);
+  process.exit(1);
+});
