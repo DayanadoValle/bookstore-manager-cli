fe73a45fa22de19f800f8dcd27b96342a97949f5
DayanadoValle <dayanadovalle@hotmail.com>
2026-07-20 15:49:59 -0300

    feat: atualiza modelo de Autor

--- src/models/Autor.ts
+++ src/models/Autor.ts
@@ -1,65 +1,69 @@
-
 export interface AutorDTO {
-    nome: string;
-    nacionalidade?: string;
-    dataNascimento?: string;
+  nome: string;
+  nacionalidade?: string;
+  dataNascimento?: string;
 }
 
 export class Autor {
-    private _id: number;
-    private _nome: string;
-    private _nacionalidade?: string;
-    private _dataNascimento?: string;
-    private _criadoEm?: Date;
+  private _id: number;
+  private _nome: string;
+  private _nacionalidade?: string;
+  private _dataNascimento?: string;
+  private _criadoEm?: Date;
 
-    constructor(
-        id: number,
-        nome: string,
-        nacionalidade?: string,
-        dataNascimento?: string,
-        criadoEm?: Date
-    ) {
-        this._id = id;
-        this._nome = nome;
-        this._nacionalidade = nacionalidade;
-        this._dataNascimento = dataNascimento;
-        this._criadoEm = criadoEm;
-    }
+  constructor(
+    id: number,
+    nome: string,
+    nacionalidade?: string,
+    dataNascimento?: string,
+    criadoEm?: Date,
+  ) {
+    this._id = id;
+    this._nome = nome;
+    this._nacionalidade = nacionalidade;
+    this._dataNascimento = dataNascimento;
+    this._criadoEm = criadoEm;
+  }
 
-    get id(): number {
-        return this._id;
-    }
+  get id(): number {
+    return this._id;
+  }
 
-    get nome(): string {
-        return this._nome;
-    }
+  get nome(): string {
+    return this._nome;
+  }
 
-    set nome(nome: string) {
-        this._nome = nome;
-    }
+  set nome(nome: string) {
+    this._nome = nome;
+  }
 
-    get nacionalidade(): string | undefined {
-        return this._nacionalidade;
-    }
+  get nacionalidade(): string | undefined {
+    return this._nacionalidade;
+  }
 
-    set nacionalidade(nacionalidade: string | undefined) {
-        this._nacionalidade = nacionalidade;
-    }
+  set nacionalidade(nacionalidade: string | undefined) {
+    this._nacionalidade = nacionalidade;
+  }
 
-    get dataNascimento(): string | undefined {
-        return this._dataNascimento;
-    }
+  get dataNascimento(): string | undefined {
+    return this._dataNascimento;
+  }
 
-    set dataNascimento(dataNascimento: string | undefined) {
-        this._dataNascimento = dataNascimento;
-    }
+  set dataNascimento(dataNascimento: string | undefined) {
+    this._dataNascimento = dataNascimento;
+  }
 
-    get criadoEm(): Date | undefined {
-        return this._criadoEm;
-    }
+  get criadoEm(): Date | undefined {
+    return this._criadoEm;
+  }
 
-   
-    static fromRow(row: any): Autor {
-        return new Autor(row.id, row.nome, row.nacionalidade, row.data_nascimento, row.criado_em);
-    }
+  static fromRow(row: any): Autor {
+    return new Autor(
+      row.id,
+      row.nome,
+      row.nacionalidade,
+      row.data_nascimento,
+      row.criado_em,
+    );
+  }
 }
